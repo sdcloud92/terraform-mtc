81181bb0c5363cf55a3eab6ee376d18803292ccf
Ubuntu <ubuntu@ip-172-31-59-240.ec2.internal>
2024-02-05 18:51:24 +0000

    using the for loop expression in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,9 @@
-output "container_ip_address" {
-  value       = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
-  description = "The IP address of the container"
-}
-
 output "container_name" {
   value       = docker_container.nodered_container[*].name
   description = "Name of container"
 }
-output "container_ip_address2" {
-  value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
+output "container_ip_address" {
+  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
   description = "The IP address of the container"
 }
 
