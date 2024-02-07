36e163d6f9e345004e80e54bd62d555ad6b3682c
Ubuntu <ubuntu@ip-172-31-59-240.ec2.internal>
2024-02-07 18:42:00 +0000

    added custom validation rules for variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,40 @@
 variable "ext_port" {
-  default = 1880
   type    = number
+  default = 1880
+
+  validation {
+    condition     = var.ext_port <= 65535 && var.ext_port > 0
+    error_message = "The port must be within the valid port range of 0 and 1880"
+  }
 }
 
 variable "int_port" {
-  default = 1880
   type    = number
+  default = 1880
+
+  validation {
+    condition     = var.int_port == 1880
+    error_message = "Internal port must be 1880"
+  }
 }
 
 variable "container_count" {
-  default = 1
   type    = number
+  default = 1
+
 }
 
 variable "random_string_count" {
-  default = 1
   type    = number
+  default = 1
 }
 
 variable "random_string_length" {
-  default = 4
   type    = number
+  default = 4
 }
 
 variable "container_name" {
-  default = "nodered"
   type    = string
+  default = "nodered"
 }
